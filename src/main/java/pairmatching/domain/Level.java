a7457699e693e64fd64a44fd4de9a16ba67a7fac
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 14:56:37 +0900

    feat: Level 클래스 정적 static 메서드 생성
    해당하는 레벨이 없을 경우 예외 처리

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,12 @@
 package pairmatching.domain;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -11,5 +18,18 @@ public enum Level {
 
     Level(String name) {
         this.name = name;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private static final Map<String, Level> menus =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(Level::getName, Function.identity())));
+
+    public static Level from(String number) {
+        return Optional.ofNullable(menus.get(number)).orElseThrow(
+                () -> new IllegalArgumentException("올바른 레벨을 입력해주세요."));
     }
 }
