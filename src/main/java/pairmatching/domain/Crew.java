1a869c9762ece169f569ee537cf221b2a71b8b23
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 14:20:25 +0900

    feat: 페어 매칭에 필요한 크루들의 이름불러오기

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -3,4 +3,21 @@ package pairmatching.domain;
 public class Crew {
     private Course course;
     private String name;
+
+    private Crew(Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+
+    public static Crew of(Course course, String name) {
+        return new Crew(course, name);
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/pairmatching/repository/CrewRepository.java
+++ src/main/java/pairmatching/repository/CrewRepository.java
@@ -1,4 +1,31 @@
 package pairmatching.repository;
 
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CrewRepository {
+    private static List<Crew> crews = new ArrayList<>();
+
+    public void addCrew(Crew crew) {
+        this.crews.add(crew);
+    }
+
+    public List<String> readBackendCrews() {
+        return crews.stream()
+                .filter(crew -> crew.getCourse() == Course.BACKEND)
+                .map(Crew::getName)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> readFrontendCrews() {
+        return crews.stream()
+                .filter(crew -> crew.getCourse() == Course.FRONTEND)
+                .map(Crew::getName)
+                .collect(Collectors.toList());
+    }
 }
