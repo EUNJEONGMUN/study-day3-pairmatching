c1804dc776d2311a81d6a9fdb6b7e2c361226edf
EUNJEONGMUN <munej26@gmail.com>
2022-12-16 01:01:27 +0900

    fix: 짝수 확인 부분 조건문 수정

--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -62,7 +62,7 @@ public class PairMatchingService {
         for (int i = 0; i < (names.size() / 2) * 2; i += 2) {
             result.add(Pair.of(Crew.of(course, names.get(i)), Crew.of(course, names.get(i + 1))));
         }
-        if (names.size() != 0) {
+        if (names.size() % 2 != 0) {
             result.get(result.size() - 1)
                     .addCrew(Crew.of(course, names.get(names.size() - 1)));
         }
