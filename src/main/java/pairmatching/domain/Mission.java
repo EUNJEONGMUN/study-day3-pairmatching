32dbb6a197c52e73051aa2ef1240438d6f22bd8e
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 13:46:11 +0900

    feat: 제공된 과정, 레벨, 미션 생성

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,4 +1,20 @@
 package pairmatching.domain;
 
 public enum Mission {
+    RACING_CAR(Level.LEVEL1, "자동차 경주"),
+    LOTTO(Level.LEVEL1, "로또"),
+    BASEBALL(Level.LEVEL1, "숫자야구게임"),
+    SHOPPING(Level.LEVEL2, "장바구니"),
+    PAYMENT(Level.LEVEL2, "결제"),
+    SUBWAY(Level.LEVEL2, "지하철노선도"),
+    REFACTORING(Level.LEVEL4, "성능개선"),
+    RELEASE(Level.LEVEL4, "배포");
+
+    private final Level level;
+    private final String name;
+
+    Mission(Level level, String name) {
+        this.level = level;
+        this.name = name;
+    }
 }
