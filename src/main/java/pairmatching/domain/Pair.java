0592ab2eeb1f494870661af82af7f0c49c496a74
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 15:14:49 +0900

    feat: 페어가 된 크루들의 이름을 담은 Pair 클래스 생성

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -1,4 +1,17 @@
 package pairmatching.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Pair {
+    private final List<Crew> names = new ArrayList<>();
+
+    private Pair(List<Crew> names) {
+        this.names.addAll(names);
+    }
+
+    public static Pair of(Crew ... names) {
+        return new Pair(Arrays.asList(names));
+    }
 }
