2a660caa65a0a9742b9c0ad94594243cce8b1fc8
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 14:27:10 +0900

    feat: 기능의 종료를 출력하고 입력받는다.

--- src/main/java/pairmatching/domain/Menu.java
+++ src/main/java/pairmatching/domain/Menu.java
@@ -1,4 +1,34 @@
 package pairmatching.domain;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Menu {
+    PAIR_MATCHING("1"),
+    LOOKUP_PAIR("2"),
+    INIT_PAIR("3"),
+    QUIT("Q");
+
+    private final String code;
+
+    Menu(String code) {
+        this.code = code;
+    }
+
+    private String getCode() {
+        return code;
+    }
+
+    private static final Map<String, Menu> menus =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(Menu::getCode, Function.identity())));
+
+    public static Menu from(String number) {
+        return Optional.ofNullable(menus.get(number)).orElseThrow(
+                () -> new IllegalArgumentException("선택할 수 없는 기능입니다."));
+    }
 }
