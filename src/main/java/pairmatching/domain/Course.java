7b0f1ac1045abf1a46456407b5ffedeae361ac92
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 14:56:17 +0900

    feat: Course 클래스 정적 static 메서드 생성
    해당하는 코스가 없을 경우 예외 처리

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,5 +1,12 @@
 package pairmatching.domain;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Course {
     BACKEND("벡엔드"),
     FRONTEND("프론트엔드");
@@ -8,5 +15,18 @@ public enum Course {
 
     Course(String name) {
         this.name = name;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private static final Map<String, Course> menus =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(Course::getName, Function.identity())));
+
+    public static Course from(String number) {
+        return Optional.ofNullable(menus.get(number)).orElseThrow(
+                () -> new IllegalArgumentException("올바른 과정을 입력해주세요."));
     }
 }
