6aca4e9503771c6908afe8277c619a1a6860936a
EUNJEONGMUN <munej26@gmail.com>
2022-12-15 15:15:16 +0900

    feat: 페어 매칭을 한 기록이 있는지 확인

--- src/main/java/pairmatching/repository/PairRepository.java
+++ src/main/java/pairmatching/repository/PairRepository.java
@@ -1,4 +1,15 @@
 package pairmatching.repository;
 
+import pairmatching.domain.MissionGroup;
+import pairmatching.domain.Pair;
+
+import java.util.*;
+
 public class PairRepository {
+    private static Map<MissionGroup, List<Pair>> pairs = new HashMap<>();
+
+    public List<Pair> findByMissionGroup(MissionGroup missionGroup) {
+        return Optional.ofNullable(pairs.get(missionGroup))
+                .orElse(Collections.emptyList());
+    }
 }
--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -1,4 +1,12 @@
 package pairmatching.service;
 
+import pairmatching.domain.MissionGroup;
+import pairmatching.repository.PairRepository;
+
 public class PairMatchingService {
+    private PairRepository pairRepository = new PairRepository();
+
+    public boolean isExistsPair(MissionGroup missionGroup) {
+        return !pairRepository.findByMissionGroup(missionGroup).isEmpty();
+    }
 }
